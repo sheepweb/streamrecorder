cc97b3378998e68594ab1863bd60a8361114b8f6
Jamal Soueidan <jamal@soueidan.com>
2026-03-08 07:40:33 +0100

    fix typo

--- frontend/src/app/[locale]/shorts/layout.tsx
+++ frontend/src/app/[locale]/shorts/layout.tsx
@@ -1,4 +1,3 @@
-import { getToken } from "@/lib/token";
 import { Box } from "@mantine/core";
 import { Header } from "../(public)/components/header";
 
@@ -7,15 +6,12 @@ export default async function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const token = await getToken();
-  const isLoggedIn = !!token;
-
   return (
     <Box
       display="flex"
       style={{ flexDirection: "column", position: "relative", zIndex: 1 }}
     >
-      <Header isLoggedIn={isLoggedIn} />
+      <Header />
 
       {children}
     </Box>
